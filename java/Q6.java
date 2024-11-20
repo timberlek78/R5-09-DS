0266815916292bb6870710de336d448fbd7cdefb
Thomas <145686209+timberlek78@users.noreply.github.com>
2024-11-20 14:31:20 +0100

    Create Q6.java

---
+++ java/Q6.java
@@ -0,0 +1,14 @@
+public class Q6 {
+    public static void main(String[] args) {
+        // Conversion du paramètre en entier
+        int n = Integer.parseInt(args[0]);
+
+        // Affichage du triangle inversé d'étoiles
+        for (int i = n; i >= 1; i--) { // La boucle externe commence par la ligne la plus large
+            for (int j = 1; j <= i; j++) { // La boucle interne contrôle le nombre d'étoiles
+                System.out.print("x");
+            }
+            System.out.println(); // Passage à la ligne suivante
+        }
+    }
+}
