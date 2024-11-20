64aa150ac36429610c85907d43e7c177b297efa6
Thomas <145686209+timberlek78@users.noreply.github.com>
2024-11-20 14:27:51 +0100

    Update Q5.java

--- java/Q5.java
+++ java/Q5.java
@@ -1,14 +1,14 @@
 public class Q5 {
     public static void main(String[] args) {
-            // Conversion du paramètre en entier
-            int n = Integer.parseInt(args[0]);
-      
-            // Affichage du triangle d'étoiles
-            for (int i = 1; i <= n; i++) {
-                for (int j = 1; j <= i; j++) 
-                    System.out.print("*");
-            
-                System.out.println(); // Passage à la ligne suivante
+        // Conversion du paramètre en entier
+        int n = Integer.parseInt(args[0]);
+
+        // Affichage du triangle inversé d'étoiles
+        for (int i = n; i >= 1; i--) { // La boucle externe commence par la ligne la plus large
+            for (int j = 1; j <= i; j++) { // La boucle interne contrôle le nombre d'étoiles
+                System.out.print("*");
             }
+            System.out.println(); // Passage à la ligne suivante
+        }
     }
 }
